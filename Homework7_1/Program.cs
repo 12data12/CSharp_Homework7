25e299436ec39161ccf70ba70f6dd0385a23428c
Daria Dmitricheva <110775372+12data12@users.noreply.github.com>
2022-09-12 09:17:39 +0300

    Task1

---
+++ Homework7_1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.Write("How many lines?");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.Write("How many columns?");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            array[i, k] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            Console.Write(array[i, k] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
